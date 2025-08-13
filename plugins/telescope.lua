0f0356eb0b1639cbed50d05022cd7b4892c0d098
Gabriel Salvador <gabosalvdor2004@gmail.com>
2025-08-13 19:52:41 +0800

    remove binding for commands

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -7,7 +7,6 @@ return {
 
         vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
         vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-        vim.keymap.set("n", "<C-P>", builtin.commands, {})
         vim.keymap.set("n", "<leader>ps", function()
             builtin.live_grep()
         end, {})
