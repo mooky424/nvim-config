eba85a70e52c9975f31de581f2082786714e86fb
Gabriel Salvador <gabosalvador2004@gmail.com>
2025-08-26 12:37:59 +0800

    new remaps

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -5,7 +5,9 @@ return {
     config = function()
         local builtin = require('telescope.builtin')
 
-        vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+        vim.keymap.set("n", "<leader>pf", function()
+            builtin.find_files({ hidden = true, no_ignore = true })
+        end, {})
         vim.keymap.set("n", "<C-p>", builtin.git_files, {})
         vim.keymap.set("n", "<leader>ps", function()
             builtin.live_grep()
--- remap.lua
+++ remap.lua
@@ -1,3 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("i", "jj", "<Esc>", {desc = "Return to Normal Mode"})
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
+vim.keymap.set("n", "<leader>p", [["+p]], { desc = "Paste from system clipboard" })
+vim.keymap.set("v", "<leader>p", [["+p]], { desc = "Paste from system clipboard" })
+
